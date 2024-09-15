c80ae0a3ab040ea9e9dd438338c182484120cb01
Jiang Menghao <jj704768703@gmail.com>
2024-09-15 22:06:42 +0800

    chore: update version`1.0.7`(8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.letconst.susan"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
